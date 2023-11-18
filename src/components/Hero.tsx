24e43eb7eec41da8f674ee9fa3f1a5f4b77389a2
samanhoseinpour <samangithoseinpour@gmail.com>
2023-11-18 19:21:22 +0330

    feat:add overlay to Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,11 +5,12 @@ import { HeroProps } from '@/types';
 export default function Hero({ imgData, imgAlt, title }: HeroProps) {
   return (
     <div>
-      <h1 className="text-heading">{title}</h1>
-
       <div className="absolute -z-10 inset-0">
         <Image src={imgData} alt={imgAlt} className="object-cover" fill />
+        <div className="absolute inset-0 bg-gradient-to-r from-slate-800"></div>
       </div>
+
+      <h1 className="text-heading">{title}</h1>
     </div>
   );
 }
