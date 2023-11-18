90d681a5a3eea797d5167a9b8bd9dcdf6df57052
samanhoseinpour <samangithoseinpour@gmail.com>
2023-11-18 16:05:54 +0330

    fix:navbar style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,16 +4,21 @@ import { navLinks } from '@/constants';
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center px-8 py-6 text-white">
-      <Link href="/" className="font-bold text-[36px] text-blue-500">
+    <nav className="flex justify-around items-center px-8 py-6 text-white">
+      <Link
+        href="/"
+        className="font-bold text-[36px] text-blue-500 flex justify-start"
+      >
         فضای ابری
       </Link>
 
-      {navLinks.map((link) => (
-        <ul key={link.id}>
-          <Link href={link.href}>{link.title}</Link>
-        </ul>
-      ))}
+      <div className="flex justify-around items-center gap-12 font-semibold text-[18px]">
+        {navLinks.map((link) => (
+          <ul key={link.id}>
+            <Link href={link.href}>{link.title}</Link>
+          </ul>
+        ))}
+      </div>
     </nav>
   );
 }
