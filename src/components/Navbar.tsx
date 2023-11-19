8a6f2389e2fa19636d1c9de79a3c53f7f138ce2c
samanhoseinpour <samangithoseinpour@gmail.com>
2023-11-19 14:50:07 +0330

    make navbar responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,15 +4,15 @@ import { navLinks } from '@/constants';
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-around items-center px-8 py-6 text-white">
+    <nav className="container mx-auto w-full flex flex-wrap justify-around items-center p-8 text-white whitespace-nowrap overflow-hidden z-10">
       <Link
         href="/"
-        className="font-bold text-[36px] text-blue-500 flex justify-start"
+        className="font-bold max-sm:text-[22px] text-[36px] text-blue-500 flex justify-start"
       >
         فضای ابری
       </Link>
 
-      <div className="flex justify-around items-center gap-12 font-semibold text-[18px]">
+      <div className="flex justify-around items-center gap-12 font-semibold text-xl max-sm:mt-[12px]">
         {navLinks.map((link) => (
           <ul key={link.id}>
             <Link href={link.href}>{link.title}</Link>
